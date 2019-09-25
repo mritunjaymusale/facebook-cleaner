631d81d0b4f8393320f219467704f1d70f3e741e
mritunjaymusale <mritunjaymusale@gmail.com>
2019-09-26 00:23:07 +0530

    just added js blocking code

--- like-and-reaction-remover.js
+++ like-and-reaction-remover.js
@@ -1,4 +1,4 @@
-const { clickEditOption,loginFromCookie } = require("./utils");
+const { clickEditOption, loginFromCookie } = require("./utils");
 
 const puppeteer = require('puppeteer');
 const process = require('process');
@@ -8,13 +8,25 @@ const process = require('process');
 
 (async () => {
     // insert respective links 
-     const browser = await puppeteer.launch({
+    const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null
     });
     const page = await browser.newPage();
+    await page.setRequestInterception(true);
+
+    // blocks css,fonts and js  from loading
+    page.on('request', (req) => {
+        if ( req.resourceType() == 'font' ||req.resourceType() === 'image') {
+            req.abort();
+        }
+        else {
+            req.continue();
+        }
+    });
     await loginFromCookie(page);
     await page.goto(process.argv[2], { waitUntil: 'networkidle2' });
+    await page.setDefaultTimeout(0);
     while (true)
         await unlike(page);
 
